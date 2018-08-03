e8ee292014fb6f5ca7dd875a258a1334be33b7ff
Johan Aronsson <aronsson.86@gmail.com>
2018-08-03 13:33:41 +0200

    implemented map based services, closes #6

---
+++ PetClinic-Data/src/main/java/com/aronsson/petclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package com.aronsson.petclinic.services.map;
+
+import com.aronsson.petclinic.model.Pet;
+import com.aronsson.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapServices<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
