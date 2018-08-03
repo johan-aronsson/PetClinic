081ca5028a335e05970729f3a4514e0bb7b70bd6
Johan Aronsson <aronsson.86@gmail.com>
2018-08-03 13:08:19 +0200

    refactored services with common interface CrudServices, closes #11

--- PetClinic-Data/src/main/java/com/aronsson/petclinic/services/PetService.java
+++ PetClinic-Data/src/main/java/com/aronsson/petclinic/services/PetService.java
@@ -2,13 +2,7 @@ package com.aronsson.petclinic.services;
 
 import com.aronsson.petclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long>{
 
-public interface PetService {
 
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
